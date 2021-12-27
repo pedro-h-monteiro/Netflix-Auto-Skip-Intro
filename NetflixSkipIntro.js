132bcb9fa795d1a823e927aac4d4af47309858ee
Pedro Monteiro <69516131+pedro-h-monteiro@users.noreply.github.com>
2021-12-27 20:27:53 +0000

    Add files via upload

---
+++ NetflixSkipIntro.js
@@ -0,0 +1,51 @@
+// ==UserScript==
+// @name         Netflix Skip Intro
+// @namespace    netflix_skip_intro
+// @version      1.0
+// @description  Automaticly skip Netflix intro
+// @author       Pedro Monteiro
+// @match        *://www.netflix.com/*
+// @grant        none
+// ==/UserScript==
+
+(function()
+{
+    var timeout = 5000;
+    var interval;
+    interval = setInterval(function()
+    {
+        lookForSkip();
+    },timeout);
+
+    function lookForSkip()
+    {
+        var video = document.getElementsByTagName("video")[0];
+        var skipButton = document.getElementsByClassName("button-primary watch-video--skip-content-button medium hasLabel ltr-ublg01")[0];
+        console.log("I'm ALIVEEEEEEE");
+        if (typeof skipButton != "undefined" && video != "undefined")
+        {
+            skipButton.click();
+            console.log('skip');
+            clearInterval(interval)
+        }
+    }
+
+    let lastUrl = location.href;
+    new MutationObserver(() =>
+    {
+        const url = location.href;
+        if (url !== lastUrl)
+        {
+            lastUrl = url;
+            onUrlChange();
+        }
+    }).observe(document, {subtree: true, childList: true});
+
+    function onUrlChange()
+    {
+        console.log("url changed");
+        interval = setInterval(function(){
+            lookForSkip();
+        },timeout);
+    }
+})();
